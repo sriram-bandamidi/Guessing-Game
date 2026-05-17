7a97d978ecb3e72484fb93bd22c57809c35f045e
srirambandamidi <srirambandamidi011@gmail.com>
2026-05-17 21:12:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Guessing Game
+A simple interactive number guessing game built using HTML, CSS, and JavaScript.
---
+++ script.js
@@ -0,0 +1,22 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 100);
+console.log(randomNumber)
+
+function checkGuess() {
+    let guessedNumber = parseInt(userInput.value);
+    if (guessedNumber > randomNumber) {
+        gameResult.textContent = "Too High! Try Again.";
+        gameResult.style.backgroundColor = "#1e217c";
+    } else if (guessedNumber < randomNumber) {
+        gameResult.textContent = "Too Low! Try Again.";
+        gameResult.style.backgroundColor = "#1e217c";
+    } else if (guessedNumber === randomNumber) {
+        gameResult.textContent = "Congradulations you got it right!";
+        gameResult.style.backgroundColor = "green";
+    } else {
+        gameResult.textContent = "Provide a valid user input";
+        gameResult.style.backgroundColor = "#1e217c";
+    }
+
+}
